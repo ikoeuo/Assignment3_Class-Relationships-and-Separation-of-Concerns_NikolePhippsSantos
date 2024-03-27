78d2213534d2acb1cd0664c32468d8090469761d
nick <146023067+ikoeuo@users.noreply.github.com>
2024-03-27 03:39:55 -0400

    part 1 done

---
+++ Program.py
@@ -0,0 +1,16 @@
+def showMainMenu():
+    print('What would you like to do?')
+    print('1. Open Account')
+    print('2. Select account')
+    print('3. Exit')
+
+def showAccountMenu():
+    print('What would you like to do?')
+    print('1. Check Balance')
+    print('2. Deposit')
+    print('3. Withdraw')
+    print('4. Exit Account')
+
+def run():
+    pass
+
