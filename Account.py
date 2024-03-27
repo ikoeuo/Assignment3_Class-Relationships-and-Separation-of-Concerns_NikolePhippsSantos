52d0b61170e1db8ae8842dce36e09c20289c7393
nick <146023067+ikoeuo@users.noreply.github.com>
2024-03-27 04:23:15 -0400

    part 2 done

---
+++ Account.py
@@ -0,0 +1,83 @@
+class Account:
+    def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance):
+        self.accountNumber = accountNumber
+        self.accountHolderName = accountHolderName
+        self.rateOfInterest = rateOfInterest
+        self.currentBalance = currentBalance
+
+    def getAccountNumber(self):
+        return self.accountNumber
+    
+    def getAccountHolderName(self):
+        return self.accountHolderName
+    
+    def getRateOfInterest(self):
+        return self.rateOfInterest
+    
+    def getCurrentBalance(self):
+        return self.currentBalance
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.currentBalance += amount
+            print(f'Deposit  successful, Your new balance is: {self.currentBalance}')
+        else:
+            print('Invalid deposit amount')
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.currentBalance:
+            self.currentBalance -= amount
+            print(f'Widthdrawl successful, Your new balance is: {self.currentBalance}.')
+        else:
+            print('Invalid deposit amount')
+
+class SavingsAccount:
+    def __init__(self):
+        self.currentBalance = 0
+    def withdraw(self, amount):
+        self.currentBalance = amount
+        self.minimumBalance = 1000
+        self.currentBalance -= amount
+
+        if self.currentBalance < self.minimumBalance:
+            print('Unable to widthdraw amount')
+            self.currentBalance += amount
+
+        elif amount > 0 and self.currentBalance >= self.minimumBalance:
+            print(f'Sucessfully widthdrew {amount}, new balance is {self.currentBalance}')
+
+        else:
+            print('Error please try again.')
+
+class ChecquingAccount:
+    def __init__(self):
+            self.currentBalance = 0
+    def withdraw(self, amount):
+        self.currentBalance = amount
+        self.overdraftAllowed = -50
+        self.currentBalance -= amount
+
+        if self.currentBalance < self.overdraftAllowed:
+            print('Unable to widthdraw amount')
+            self.currentBalance += amount
+
+        elif amount > 0 and self.currentBalance >= self.overdraftAllowed:
+            print(f'Sucessfully widthdrew {amount}, new balance is {self.currentBalance}')
+
+        else:
+            print('Error please try again.')
+
+
+class Bank:
+    def __init__(self, bankName):
+        self.bankName = bankName 
+        self.accounts = []
+
+    def openAccount(self, account):
+        self.accounts.append(account)
+        print(f'Your new bank account {Account.getAccountNumber()} has been opened!')
+
+    def searchAccount(self, accountNumber):
+        for account in self.accounts:
+            if Account.getAccountNumber == accountNumber:
+                return account
