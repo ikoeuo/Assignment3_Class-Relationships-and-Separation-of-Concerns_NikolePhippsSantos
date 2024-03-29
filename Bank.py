90e7d5caa4f629f178b5cd0cb818348e3764fddf
nick <146023067+ikoeuo@users.noreply.github.com>
2024-03-28 22:21:03 -0400

    Finally done :D

--- Bank.py
+++ Bank.py
@@ -1,9 +1,11 @@
 class Account:
-    def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance):
+    def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance, overdraftAllowed):
         self.accountNumber = accountNumber
         self.accountHolderName = accountHolderName
         self.rateOfInterest = rateOfInterest
         self.currentBalance = currentBalance
+        self.minimumBalance = minimumBalance
+        self.overdraftAllowed = overdraftAllowed
 
     def getAccountNumber(self):
         return self.accountNumber
@@ -20,76 +22,65 @@ class Account:
     def deposit(self, amount):
         if amount > 0:
             self.currentBalance += amount
-            print(f'Deposit  successful, Your new balance is: {self.currentBalance}')
+            print(f'Deposit successful, Your new balance is: {self.currentBalance}')
         else:
             print('Invalid deposit amount')
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self.currentBalance:
             self.currentBalance -= amount
-            print(f'Widthdrawl successful, Your new balance is: {self.currentBalance}.')
+            print(f'Withdrawal successful, Your new balance is: {self.currentBalance}.')
         else:
-            print('Invalid deposit amount')
+            print('Invalid withdrawal amount')
 
-class SavingsAccount:
+class SavingsAccount(Account):
     def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance):
-        super().__init__(accountNumber, accountHolderName, rateOfInterest, currentBalance)
-        self.currentBalance = currentBalance
-        self.minimumBalance = minimumBalance
+        super().__init__(accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance, 0)
+
     def withdraw(self, amount):
-        self.currentBalance = amount
         self.currentBalance -= amount
-  
-        
         if self.currentBalance < self.minimumBalance:
-            print('Unable to widthdraw amount')
+            print('Unable to withdraw amount')
             self.currentBalance += amount
-
         elif amount > 0 and self.currentBalance >= self.minimumBalance:
-            print(f'Sucessfully widthdrew {amount}, new balance is {self.currentBalance}')
-
+            print(f'Successfully withdrew {amount}, new balance is {self.currentBalance}')
         else:
             print('Error please try again.')
 
-class ChecquingAccount:
+class ChequingAccount(Account):
     def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance, overdraftAllowed):
-            super().__init__(accountNumber, accountHolderName, rateOfInterest, currentBalance)
-            self.currentBalance = currentBalance
-            self.overdraftAllowed = overdraftAllowed
+        super().__init__(accountNumber, accountHolderName, rateOfInterest, currentBalance, 0, overdraftAllowed)
+
     def withdraw(self, amount):
-        self.currentBalance = amount
         self.currentBalance -= amount
-
         if self.currentBalance < self.overdraftAllowed:
-            print('Unable to widthdraw amount')
+            print('Unable to withdraw amount')
             self.currentBalance += amount
-
         elif amount > 0 and self.currentBalance >= self.overdraftAllowed:
-            print(f'Sucessfully widthdrew {amount}, new balance is {self.currentBalance}')
-
+            print(f'Successfully withdrew {amount}, new balance is {self.currentBalance}')
         else:
             print('Error please try again.')
 
-
-class Bank:
-    def __init__(self, bankName, account):
+class Bank(SavingsAccount, ChequingAccount, Account):
+    def __init__(self, bankName):
         self.bankName = bankName 
         self.accounts = []
-        self.account = account
 
-    def openChequingAccount(self, bankName, accountNumber, accountHolderName, rateOfInterest, currentBalance, overdraftAllowed):
-        super().__init__(bankName, accountNumber, accountHolderName, rateOfInterest, currentBalance, overdraftAllowed)
-        self.accounts.append(self.account)
-        print(f'Your new bank account {Account.getAccountNumber()} has been opened!')
+    def openChequingAccount(self, accountNumber, accountHolderName, rateOfInterest, currentBalance, overdraftAllowed):
+        account = ChequingAccount(accountNumber, accountHolderName, rateOfInterest, currentBalance, overdraftAllowed)
+        self.accounts.append(account)
+        print(f'Your new bank account {account.getAccountNumber()} has been opened!')
+        return account
 
-    def openSavingsAccount(self, bankName, accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance):
-        super().__init__(bankName, accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance)
-        self.accounts.append(self.account)
-        print(f'Your new bank account {Account.getAccountNumber()} has been opened!')
+    def openSavingsAccount(self, accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance):
+        account = SavingsAccount(accountNumber, accountHolderName, rateOfInterest, currentBalance, minimumBalance)
+        self.accounts.append(account)
+        print(f'Your new bank account {account.getAccountNumber()} has been opened!')
+        return account
 
     def searchAccount(self, accountNumber):
         for account in self.accounts:
-            if Account.getAccountNumber == accountNumber:
+            if account.getAccountNumber() == accountNumber:
                 return account
-            else:
-                print(f'Account does not exist.')
+        print(f'Account does not exist.')
+
--- Program.py
+++ Program.py
@@ -1,72 +1,75 @@
-from Bank import Account
-from Bank import SavingsAccount
-from Bank import ChecquingAccount
-from Bank import Bank
+from Bank import Account, SavingsAccount, ChequingAccount, Bank
 
 def showMainMenu():
-    print('What would you like to do?\n1. Open Account\n2. Select account\n3. Exit')
+    print('\nWhat would you like to do?\n1. Open Account\n2. Select account\n3. Exit')
 
 def showAccountMenu():
-    print('What would you like to do?\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit Account to Main Menu')
+    print('\nWhat would you like to do?\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit Account to Main Menu')
 
 def run():
     bankName = 'RBC'
+    bank = Bank(bankName)
+
+    #adds pre made accounts, 2 of each :)
+    account1 = SavingsAccount(1010, 'Jake Perez', 0.2, 22500, 1000)
+    bank.accounts.append(account1)
+    account2 = SavingsAccount(2020, 'Verinica Smith', 0.1, 6500, 1000)
+    bank.accounts.append(account1)
+    count3 = ChequingAccount(3030, 'Jake Park', 0.05, 550, -100)
+    bank.accounts.append(account1)
+    account4 = ChequingAccount(4040, 'Finn Santos', 0.25, 1330, -100)
+    bank.accounts.append(account1)
+
 
     while True:
-
         showMainMenu()
         option = int(input('Enter choice (1-3): '))
-        
-        if option == 1: #Open Account
 
-            accountType = input('Enter account type (savings/checquing): ').lower()
-            accountNumber = int(input ('Enter account number: '))
+        if option == 1: #Open Account
+            accountType = input('Enter account type (savings/chequing): ').lower()
+            accountNumber = int(input('Enter account number: '))
             accountHolder = str(input('Enter account holder name: '))
             rateOfInterest = float(input('Enter rate of interest: %'))
-            initialBalance = float(input('Enter initial balance to deposit: '))
 
             if accountType == 'savings':
+                initialBalance = 1000
                 minimumBalance = 1000
-                account = Bank.openSavingsAccount(bankName, accountNumber, accountHolder, rateOfInterest, initialBalance , minimumBalance)   
-                
-            elif accountType == 'checquing':
-                overdraftAllowed= -100
-                account = Bank.openChequingAccount(bankName, accountNumber, accountHolder, rateOfInterest, initialBalance , overdraftAllowed)
-                
+                account = bank.openSavingsAccount(accountNumber, accountHolder, rateOfInterest, initialBalance, minimumBalance)
+            elif accountType == 'chequing':
+                initialBalance = 0
+                overdraftAllowed = -100
+                account = bank.openChequingAccount(accountNumber, accountHolder, rateOfInterest, initialBalance, overdraftAllowed)
             else:
                 print('Unable to open account, Invalid account type.')
 
-
         elif option == 2: #2. Select account
-
-            accountNumber = input('Enter Account Number: ')
-            Bank.searchAccount
-            if Bank.searchAccount == accountNumber:
+            accountNumber = int(input('Enter Account Number: '))
+            account = bank.searchAccount(accountNumber)
+            if account:
                 while True:
                     showAccountMenu()
-                    accountOption = print(input('Enter selection (1-4): '))
+                    accountOption = int(input('Enter selection (1-4): '))
 
                     if accountOption == 1: #Check Balance
-                        print('Your current balance is: ')
-                        Account.getCurrentBalance() 
+                        print('Your current balance is:', account.getCurrentBalance())
 
                     elif accountOption == 2: #2. Deposit
-                        amount = print(float(input('How much money would you like to deposit? ')))
-                        Account.deposit(amount)
+                        amount = float(input('How much money would you like to deposit? '))
+                        account.deposit(amount)
 
                     elif accountOption == 3: #3. Withdraw
-                        amount = print(float(input('How much money would you like to withdraw? ')))
-                        if accountType == 'savings':
-                            SavingsAccount.withdraw(amount)
-                        elif accountType == 'checquing':
-                            ChecquingAccount.withdraw(amount)
+                        amount = float(input('How much money would you like to withdraw? '))
+                        account.withdraw(amount)
 
                     elif accountOption == 4: #4. Exit Account to Main Menu
                         break
 
                     else:
-                        print('404')
-    
+                        print('Invalid option')
+
+            else:
+                print('Bank account does not exist.')
+
         elif option == 3: #3. Exit
             print('Thank you for using my app!')
             exit()
